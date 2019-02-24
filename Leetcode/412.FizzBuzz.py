66ec6eae072c92ac85a9981128b2a61701f94c07
Sanika Shah <sanikashah1110@gmail.com>
2019-02-24 13:27:43 -0700

    Leetcode

---
+++ Leetcode/412.FizzBuzz.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        l=[]
+        for i in range(1,n+1):
+            if i%3 == 0 and i%5 == 0:
+                l.append("FizzBuzz")
+            elif i%3 == 0:
+                l.append("Fizz")
+            elif i%5 == 0 :
+                l.append("Buzz")
+            else:
+                l.append(str(i))
+        return l
---
+++ Leetcode/669.TrimABST.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def trimBST(self, root, L, R):
+        """
+        :type root: TreeNode
+        :type L: int
+        :type R: int
+        :rtype: TreeNode
+        """
+        def trim(node):
+            if node == None:
+                return None
+#             we will got to nodes left
+            elif node.val > R:
+                return trim(node.left)
+            elif node.val < L:
+                return trim(node.right)
+            else:
+                node.left = trim(node.left)
+                node.right = trim(node.right)
+                return node
+            
+        return trim(root)
+                
+                
+            
